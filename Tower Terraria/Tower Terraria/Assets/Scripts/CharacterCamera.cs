6a0b9091b7853f733f8c3d21713adda1807f733f
Vulpire <nbw201@gmail.com>
2018-11-14 17:16:48 -0500

    First Build

---
+++ Tower Terraria/Tower Terraria/Assets/Scripts/CharacterCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CharacterCamera : MonoBehaviour {
+
+    public Transform player;
+    public float CameraDistace = 30f;
+
+    private void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height) / CameraDistace);
+    }
+
+    void FixedUpdate () {
+        transform.position = new Vector3(player.position.x,player.position.y, -20);
+	}
+}
